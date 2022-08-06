c35f4e430ce729c7fa20bcf19e83b4eab417690e
Jepoy Factoran <factoran20@gmail.com>
2022-08-06 21:47:40 +0800

    added daisyUI

--- resources/js/Layouts/Guest.vue
+++ resources/js/Layouts/Guest.vue
@@ -7,7 +7,8 @@ import { Link } from '@inertiajs/inertia-vue3';
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
             <Link href="/">
-                <BreezeApplicationLogo class="w-20 h-20 fill-current text-gray-500" />
+                <!-- <BreezeApplicationLogo class="w-20 h-20 fill-current text-gray-500" /> -->
+                <h1 class="text-2xl font-bold">Ticketing Management System</h1>
             </Link>
         </div>
 
